e36bc9c13b53041f220da6e6faa8ad7c7a5578ce
Madhav Bansal <madhavbansal2005@gmail.com>
2024-09-13 19:20:35 +0530

    placeorder

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import './Placeorder.css'
+import './PlaceOrder.css'
 import { StoreContext } from '../../context/storeContext'
 
 const PlaceOrder = () => {
